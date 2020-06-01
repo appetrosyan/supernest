f0d78b8ebcf4febd5602b71c6501a8ec1142a220
Aleksandr Petrosyan <a-p-petrosyan@Yandex.ru>
2020-06-01 20:58:28 +0400

    Small corrections.
    Took 4 minutes

--- setup.py
+++ setup.py
@@ -1,16 +1,19 @@
 """Setup code for the SSPR Package"""
 import setuptools
 
-with open("./README.md", "r") as readme:
+SHORT_DESCRIPTION='A wrapper for use of SSPR in \
+nested sampling packages such as PolyChord and Multinest'
+LONG_DESCRIPTION=SHORT_DESCRIPTION
+
+with open("./README.md") as readme:
     LONG_DESCRIPTION = readme.read()
 
 setuptools.setup(
     name='super-nest-a-p-petrosyan',
     version='0.1.0',
     author='Aleksandr Petrosyan',
-    author_email='a-p-petrosyan@Yandex.ru',
-    description='A wrapper for use of SSPR in \
-nested sampling packages such as PolyChord and Multinest',
+    author_email='a-p-petrosyan@yandex.ru',
+    description=SHORT_DESCRIPTION,
     long_description=LONG_DESCRIPTION,
     long_description_content_type='text/markdown',
     url='https://gitlab.com/a-p-petrosyan/sspr',
