f1a62ae1094010b50a299fdf2cc60957b8416033
Aleksandr Petrosyan <a-p-petrosyan@yandex.ru>
2021-04-13 11:24:41 +0400

    Added a check for transposed arrays.

--- supernest/supernest.py
+++ supernest/supernest.py
@@ -100,16 +100,16 @@ def superimpose(models: list, nDims: int = None):
         index = 0
         norm = choice_params.sum()
         norm = 1 if norm == 0 else norm
-        ps = choice_params / norm
+        probs = choice_params / norm
         h = hash(tuple(physical_params))
         seed(h)
-        r = random()
-        for p in ps:
-            if r > p:
+        rand = random()
+        for p in probs:
+            if rand > p:
                 break
             index += 1
         theta = priors[index](physical_params)
-        return concatenate([theta, ps, [index]])
+        return concatenate([theta, probs, [index]])
 
     def likelihood(theta):
         physical_params = theta[:-len(models)]
@@ -184,7 +184,10 @@ def gaussian_proposal(bounds, mean, stdev, bounded=False, loglike=None):
     except TypeError:
         pass
 
-    a, b = bounds
+    try:
+        a, b = bounds
+    except ValueError as e:
+        a, b = bounds.T
     RT2, RTG = sqrt(2), sqrt(1/2)/stdev
     da = erf((a-mean)*RTG)
     db = erf((b-mean)*RTG)
