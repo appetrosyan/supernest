08b8fa4efa1e543e5c1724b340c347a9df7e8f55
Aleksandr <a-p-petrosyan@yandex.ru>
2021-09-12 20:16:34 +0400

    version bump

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ LONG_DESCRIPTION = SHORT_DESCRIPTION
 with open("./README.md") as readme:
     LONG_DESCRIPTION = readme.read()
 
-version = "3.0.2"
+version = "3.1.2"
 
 if os.environ.get('CI_COMMIT_TAG'):
     version = os.environ['CI_COMMIT_TAG']
